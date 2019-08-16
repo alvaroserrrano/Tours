d2700e1cde35d49e23e8e2e35e082bed4d9bca0c
Alvaro <alvaro.serrano@wmich.edu>
2019-08-16 09:44:37 +0200

    signup and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,44 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const singToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-  res.status(201).json({ status: 'success', data: { user: newUser } });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  const token = singToken(newUser._id);
+
+  res.status(201).json({ status: 'success', token, data: { user: newUser } });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //1)Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide credentials', 400));
+  }
+  //2)Check if user exists and password is correct
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Invalid credentials', 401));
+  }
+  // console.log(user);
+
+  //3)If everything is ok, send token to the client
+  const token = singToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token
+  });
 });
