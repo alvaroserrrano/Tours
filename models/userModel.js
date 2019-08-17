2ee79dee9a1f672a015abd05ca116955923bc39a
Alvaro <alvaro.serrano@wmich.edu>
2019-08-17 09:40:00 +0200

    password reset functionality

--- models/userModel.js
+++ models/userModel.js
@@ -54,6 +54,12 @@ userSchema.pre('save', async function(next) {
 });
 
 //Instance method: available for all documents of a collection
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
