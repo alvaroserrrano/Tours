1f889e156002ec5b5c78c43bfba7698a2cdd466e
Alvaro <alvaro.serrano@wmich.edu>
2019-09-16 10:28:26 -0400

    overview template added

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1.Get all tours
+  const tours = await Tour.find();
+  //2.Create template
+
+  //3.Render template
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
