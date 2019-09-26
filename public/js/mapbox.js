ad706b2a360d3d0420377f4c5d8f77b1d7ac9b4a
Alvaro <alvaro.serrano@wmich.edu>
2019-09-25 20:49:29 -0400

    mapbox API

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,13 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYXNlcnJhbm8iLCJhIjoiY2swenl3ZHNzMDBkYjNicWlobXRnaDltOSJ9.DYtnUVGsoT4bzssmGcfIUA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/aserrano/ck0zz7mgh0dk91cpfpz6a0agn'
+});
