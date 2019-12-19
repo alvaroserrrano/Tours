500e8adf716de2217032e88d577abce119a46ea5
Alvaro <alvaro.serrano@wmich.edu>
2019-12-19 17:54:39 +0100

    added payment config to frontend

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { signup } from './signup';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 //DOM
 const mapBox = document.getElementById('map');
@@ -12,6 +13,7 @@ const signupForm = document.querySelector('.signup-form');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -70,4 +72,11 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
