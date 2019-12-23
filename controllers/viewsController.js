3b00726a1e88c7c908ec50fcf01d4264a3afac3e
Alvaro <alvaro.serrano@wmich.edu>
2019-12-23 17:55:53 +0100

    completed booking API and render bookings on account page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -45,6 +46,19 @@ exports.getSignupForm = (req, res) => {
 exports.getAccount = (req, res) => {
   res.status(200).render('account');
 };
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  //Find all bookings
+  const bookings = await Booking.find({ user: req.user.id });
+  //Find tours with the returned ids
+  const tourIDs = bookings.map(el => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+  res.status(200).render('overview', {
+    title: 'My tours',
+    tours
+  });
+});
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
   const updatedUser = await User.findByIdAndUpdate(
